1b49a720ee8fd9289f77f517a10f041ba23132b3
Nathan Agbayani <nathan.pagbayani@gmail.com>
2024-02-24 16:40:39 -0800

    Spinning Vinyl

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,20 @@
 // Home.js
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import RecordHome from "./RecordHome";
 
 const Home = () => {
   return (
-    <div className='flex flex-row justify-center h-[75vh]'>
-      <div className='album-cover w-[50%] h-[100%] '>
-        <div className='flex flex-wrap justify-center items-center flex-col text-white border-indigo border-4 h-[100%]'>
-          <div className='text-8xl font-aileronThin text-indigo-500'>
-            WELCOME
-            {/* <hr className='h-1 bg-indigo rounded'></hr> */}
-          </div>
-          {/* <div className='treadmill-container text-6xl'>
+    <div className='flex flex-col justify-center items-center'>
+      <div className='name font-aileronSemiBold text-8xl'>NATHAN AGBAYANI</div>
+      <RecordHome />
+    </div>
+  );
+};
+
+export default Home;
+
+/* <div className='treadmill-container text-6xl'>
             <div className='treadmill-text'>
               <ul>
                 <li>Full Stack Developer.</li>
@@ -24,33 +27,9 @@ const Home = () => {
                 <li>Creative.</li>
               </ul>
             </div>
-          </div> */}
-          <div className='text-8xl my-4 items-center font-aileronSemiBold  text-indigo-500'>
-            NATHAN AGBAYANI
-          </div>
-          <div className='flex text-2xl my-4 font-aileronSemiBold border-indigo border-4 rounded-2xl p-8  w-[25%] items-center text-indigo-500'>
-            <h1>See My Work</h1>
-          </div>
-          <div>
-            <ul className='text-3xl flex justify-between '>
-              <li className='mx-2 p-2'>
-                <Link to='/'>Home</Link>
-              </li>
-              <li className='mx-2 p-2'>
-                <Link to='/about'>About</Link>
-              </li>
-              <li className='mx-2 p-2'>
-                <Link to='/projects'>Projects</Link>
-              </li>
-              <li className='mx-2 p-2'>
-                <Link to='/experience'>Experience</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className='shadow1'></div>
-      </div>
-      {/* <div className='flex font-aileronHeavyItalic '>
+          </div> */
+
+/* <div className='flex font-aileronHeavyItalic '>
         <div className='border-indigo border-2 w-[25%]'>
           <div className='record'>
             <Link to='/'>Home</Link>
@@ -71,9 +50,4 @@ const Home = () => {
             <Link to='/experience'>Experience</Link>
           </div>
         </div>
-      </div> */}
-    </div>
-  );
-};
-
-export default Home;
+      </div> */
---
+++ src/components/RecordHome.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const RecordHome = () => {
+  return (
+    <div className='home-page flex flex-row justify-center h-[75vh]'>
+      <div className='album-cover w-[50%] h-[100%]'>
+        <div className='shadow1'></div>
+        <div className='inner-album flex justify-center flex-col items-center text-white border-indigo border-4 w-[100%] h-[100%]'>
+          <div className='text-8xl font-aileronThin text-indigo-500'>
+            WELCOME
+            {/* <hr className='h-1 bg-indigo rounded'></hr> */}
+          </div>
+          <div className='text-8xl my-4 items-center font-aileronSemiBold  text-indigo-500'>
+            NATHAN AGBAYANI
+          </div>
+          <div className='static shrink text-2xl font-aileronSemiBold -2xl p-8  items-center text-indigo-500'>
+            <div className='border-indigo border-4 mx-32 rounded'>
+              <h1 className='text-center'>See My Work</h1>
+            </div>
+          </div>
+          <div className='static shrink links'>
+            <ul className=' text-3xl flex justify-between '>
+              <li className='mx-2 p-2'>
+                <Link to='/'>Home</Link>
+              </li>
+              <li className='mx-2 p-2'>
+                <Link to='/about'>About</Link>
+              </li>
+              <li className='mx-2 p-2'>
+                <Link to='/projects'>Projects</Link>
+              </li>
+              <li className='mx-2 p-2'>
+                <Link to='/experience'>Experience</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='static overflow-visible w-[100%] h-[100%]'>
+          <div className='vinyl-cover w-[280%] h-[99%] align-bottom mr-8'>
+            <div className='vinyl w-[100%] h-[60%]'></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecordHome;
